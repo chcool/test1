3e8d128e822514a37bd251f260940522f9b8e738
unknown <hochen@SJC-HOCHEN.calix.local>
2014-11-14 20:15:27 +0000

    mylogger to unlock

--- unlock.py
+++ unlock.py
@@ -1,4 +1,17 @@
-def action(conn):
+import argparse
+import re,sys
+from optparser import parseHostlist
+from util.mylog import mylogger
+
+def action():
+    mylogger.info( "###### in unlock.action v2 ########")
+    opthash=parseHostlist()
+    if opthash.hlist:
+        mylogger.info( "hostlist = %s" % opthash.hlist)
+    else:
+        mylogger.info( "unlock:action:use -l or --hlist to give a list of host IPs")
+        sys.exit(2)
+    
 #miao
     #sendunlock command"
     pass
