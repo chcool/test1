0820b90e973cc6d971eba830dcfb5e849ffd1789
Hong Chen <hong.chen@calix.com>
2014-11-20 16:24:56 -0800

    back to linux's connect_remote.py

--- unlock.py
+++ unlock.py
@@ -1,17 +1,4 @@
-import argparse
-import re,sys
-from optparser import parseHostlist
-from util.mylog import mylogger
-
-def action():
-    mylogger.info( "###### in unlock.action v2 ########")
-    opthash=parseHostlist()
-    if opthash.hlist:
-        mylogger.info( "hostlist = %s" % opthash.hlist)
-    else:
-        mylogger.info( "unlock:action:use -l or --hlist to give a list of host IPs")
-        sys.exit(2)
-    
+def action(conn):
 #miao
     #sendunlock command"
     pass
