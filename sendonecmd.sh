ca2411f107f5458cf097767463b764178638b33a
Hong Chen <hong.chen@calix.com>
2014-12-09 17:13:35 -0800

    update sh to handle python 3

--- sendonecmd.sh
+++ sendonecmd.sh
@@ -3,18 +3,15 @@ if [ $# -lt 1 ]; then
    echo "run it like $0 <hostlist> <cmd(s)> [python version number]"
 fi
 
-if [ $# -eq 3 ]; then
-   echo "has \$3"
-   if [ $3 -eq 3 ]; then
-        echo "use python$3"
-        PYTHON="python$3"
-   else
-       echo "\$2 is not 3, still use python2"
-   fi
+last=$#
+echo "python$last"
+#exit
 
+if [ $last -eq 3 ]; then
+   echo "last param = 3,use python3"
+   PYTHON="python$3"
 else
    echo "use python2"
 fi
 
-#eval "$PYTHON climain.py -a sendcmd.action -m 'show ver' -l $1"
 eval "$PYTHON climain.py -a sendcmd.action -m \"$2\" -l \"$1\""
