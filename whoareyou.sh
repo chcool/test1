3b346cf95ce6607c923cb2912a3be03f04670dd4
Hong Chen <hong.chen@calix.com>
2014-12-16 10:19:51 -0800

    add whoareyou.sh

---
+++ whoareyou.sh
@@ -0,0 +1,17 @@
+PYTHON=python
+if [ $# -lt 1 ]; then
+   echo "run it like $0 <hostlist> <cmd(s)> [python version number]"
+fi
+
+last=$#
+echo "python${!last}"
+#exit
+
+if [ ${!last} -eq 3 ]; then
+   echo "last param = 3,use python3"
+   PYTHON="python${!last}"
+else
+   echo "use python2"
+fi
+# change the -a and other options
+eval "$PYTHON climain.py -a sendcmd.action -m \"shell,whoareyou.sh,@exp '<Ctl-C> to exit',ctrl-c\" -l \"$1\""
